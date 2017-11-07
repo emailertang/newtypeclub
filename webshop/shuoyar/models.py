b21642d7f09f5f0b40bf0c91777557bfe5fd07ee
shuoyar <emailertang@foxmail.com>
2017-11-07 23:26:17 +0800

    20171107

--- webshop/shuoyar/models.py
+++ webshop/shuoyar/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 class Publisher(models.Model):
     name = models.CharField(max_length=20)
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=50,null=True)
     city = models.CharField(max_length=10,null=True)
     state_provice = models.CharField(max_length=10,null=True)
     contry = models.CharField(max_length=10,null=True)
@@ -16,16 +16,16 @@ class Publisher(models.Model):
 
 class Auther(models.Model):
     name = models.CharField(max_length=30)
-    age = models.IntegerField()
-    email = models.EmailField()
+    age = models.IntegerField(null=True)
+    email = models.EmailField(null=True)
     def __str__(self):
         return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Auther)
-    publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    authors = models.ManyToManyField(Auther,null=True)
+    publisher = models.ForeignKey(Publisher,null=True)
+    publication_date = models.DateField(auto_now_add=True)
     def __str__(self):
         return self.title
 
