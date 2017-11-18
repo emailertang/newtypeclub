78153b18fcb611430c8969f7735eb81a968890b1
shuoyar <emailertang@foxmail.com>
2017-11-18 19:30:54 +0800

    123
    123

--- webshop/shuoyar/models.py
+++ webshop/shuoyar/models.py
@@ -15,7 +15,6 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
 
-class Auther(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField(null=True)
